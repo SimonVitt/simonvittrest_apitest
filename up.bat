e629c5c9473d9bd2e061c859c400da5772995687
Simon Vitt <ttivnomis@gmail.com>
2023-02-08 18:35:28 +0100

    first workin version

--- up.bat
+++ up.bat
@@ -1,4 +1,4 @@
 git add .
 git commit -m "%*"
 git push
-chrome "https://www.pythonanywhere.com/user/simonvitttest/consoles/27284772/"
+chrome.exe "https://www.pythonanywhere.com/user/simonvitttest/consoles/27284772/"
